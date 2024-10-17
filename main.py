92f01fcc4f1b859286b4146061a04c33e0914cef
falcaoanderson <andersonfalcaosantos@gmail.com>
2024-10-16 23:44:58 -0300

    alteração na main para utilizar a classe Interface

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.app.printdata import PrintData
+from src.app.interface import Interface
+from src.utils.console_utils import clean_screen
 import os 
 from dotenv import load_dotenv
 
@@ -8,13 +10,25 @@ def main():
     print_data = PrintData("vmdb", os.getenv("DB_USER"), os.getenv("DB_PASSWORD"), "localhost", "5432")
     print_data.connect()
 
-    print_data.display_vm_list()
+    inteface = Interface(print_data)
 
-    vm_id = input("Enter Vending Machine ID to view details: ")
-    print("\n")
+    while True:
+        clean_screen()
+        while True:
+            type_user = input("Enter '1' for User or '2' for Manager [or -1 to exit]: ").strip()
+
+            if type_user in ["1", "2", "-1"]:
+                break
+            else:
+                print("Invalid option, please enter '1' for User or '2' for Manager.")
+
+        if type_user == "1":
+            inteface.searchMenu()
+        elif type_user == "2": 
+            inteface.stockMenu()
+        else:   
+            break
     
-    print_data.display_vm_details(vm_id)
-
     print_data.shutdown()
 
 if __name__ == "__main__":
